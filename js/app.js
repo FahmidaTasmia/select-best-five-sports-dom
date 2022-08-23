6bdc958ad4d0a597fd52edb331c27d1d1685aeb2
FahmidaTasmia <tasmia1070@gmail.com>
2022-08-23 08:54:40 +0600

    cut unneccessary thing

--- js/app.js
+++ js/app.js
@@ -39,26 +39,3 @@ function addToCart(event) {
 
 
 
-// function updatePhoneNumber (isIncrease){
-//     const phoneNumberField = document.getElementById('phone-number-field');
-//     const phoneNumberString = phoneNumberField.value;
-//     const previousPhoneNumber = parseInt(phoneNumberString);
-
-//     let newPhoneNumber;
-
-//     if(isIncrease){
-//         newPhoneNumber = previousPhoneNumber + 1;
-//     }
-//     else{
-//         newPhoneNumber = previousPhoneNumber - 1;
-//     }
-//     phoneNumberField.value = newPhoneNumber;
-
-//     return newPhoneNumber;
-// }
-
-// function updatePhoneTotalPrice(newPhoneNumber){
-//     const phoneTotalPrice = newPhoneNumber * 1219;
-//     const phoneTotalElement = document.getElementById('phone-total');
-//     phoneTotalElement.innerText = phoneTotalPrice;
-// }
